1e6cc374b45c90e0aa682801f4273f9b955e7d51
Andrey Nabiullin <itcomusic@gmail.com>
2023-01-19 12:27:10 +0300

    example

---
+++ example_test.go
@@ -0,0 +1,47 @@
+package redismutex_test
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/go-redis/redis/v8"
+
+	"github.com/itcomusic/redismutex"
+)
+
+func Example() {
+	// connect to redis
+	rc := redis.NewClient(&redis.Options{
+		Network: "tcp",
+		Addr:    "127.0.0.1:6379",
+	})
+	defer rc.Close()
+
+	// init mutex
+	mx := redismutex.NewMutex(rc, "mutex_name")
+
+	// lock, optionally add key and lock_key will be "mutex_name:key"
+	lock, ok := mx.Lock(redismutex.WithKey("key")) // also available RLock, TryLock, TryRLock
+	if !ok {
+		log.Fatalln("could not obtain lock, disconnected from redis?")
+	}
+	defer lock.Unlock()
+
+	handler := func(ctx context.Context) {
+		select {
+		case <-ctx.Done():
+			fmt.Println("lock expired")
+
+		default:
+			fmt.Println("lock active")
+		}
+	}
+
+	fmt.Printf("lock %q!\n", lock.Key())
+	handler(lock) // lock implements interface context.Context
+
+	// Output:
+	// lock "mutex_name:key"!
+	// lock active
+}
